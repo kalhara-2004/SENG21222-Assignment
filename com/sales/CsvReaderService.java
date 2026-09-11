3feb1f1114509d7ee4a5b3fa1f72d918bb436e2d
Yasiru Munasingha <yasirusandeepaofficial@gmail.com>
2026-09-11 22:13:10 +0530

    Update CsvReaderService class

--- com/sales/CsvReaderService.java
+++ com/sales/CsvReaderService.java
@@ -1,4 +1,39 @@
 package com.sales;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CsvReaderService {
+
+    public List<Product> readProductsFromCsv(String filePath) throws
+            IOException {
+        List<Product> products = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath)))
+        {
+            String line;
+            boolean isHeader = true;
+
+            while ((line = br.readLine()) != null) {
+                if (line.trim().isEmpty()) {
+                    continue;
+                }
+
+                if (isHeader) {
+                    isHeader = false; // Skip the header row
+                    continue;
+                }
+
+                String[] tokens = line.split(",");
+                if (tokens.length < 5) {
+                    throw new IllegalArgumentException("Invalid CSV row format: " + line);
+                }
+            }
+
+        }
+
+    }
 }
