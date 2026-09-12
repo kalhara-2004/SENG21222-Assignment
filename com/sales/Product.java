1f78ea86889ed587c336bd70c1a26306599e6b93
kawi2004 <kawishaanujith2004pkv@gmail.com>
2026-09-12 13:06:06 +0530

    Add Product class with attributes and constructor

--- com/sales/Product.java
+++ com/sales/Product.java
@@ -1,5 +1,19 @@
 package com.sales;
 
 public class Product {
+
+    private final String productId;
+    private final String productName;
+    private final String category;
+    private final int quantitySold;
+    private final double unitPrice;
+
+    public Product(String productId, String productName, String category, int quantitySold, double unitPrice) {
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+        this.quantitySold = quantitySold;
+        this.unitPrice = unitPrice;
+    }
     
 }
